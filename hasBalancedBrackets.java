ade8e7b40c85e7b13ee0fcb989af5a521135e892
Stasa <stasarasic@gmail.com>
2017-12-09 18:44:35 +0100

    Create hasBalancedBrackets.java

---
+++ hasBalancedBrackets.java
@@ -0,0 +1,39 @@
+/** Created by Stasa on 24.11.17.. **/
+
+import java.util.Stack;
+
+public class Brackets{
+
+public static boolean hasBalancedBrackets(String str){
+
+    Stack<Character> stack = new Stack<Character>();
+    char x, y;
+
+    for(int i = 0; i < str.length(); i++){
+       x = str.charAt(i);
+
+       if ( x == '(' || x == '[' || x == '{' || x == '<' )
+            stack.push(x);
+
+       if ( x == ')' || x == ']' || x == '}' || x == '>') {
+            if (stack.empty())
+            return false;
+               
+            else {
+                y = stack.pop();
+                if(x - y != 2 && x - y != 1)
+                   return false;
+            }
+      }
+   }
+
+    return stack.empty() ? true : false;
+}
+
+public static void main(String[] args)
+{
+    System.out.println("Brackets are " + ( hasBalancedBrackets("{([kglfk{<hhii>}ngccdfnj])ppll{}[]}") ? "is" : "is not" ) + " balanced" );
+
+  
+    System.out.println("Brackets are " + ( hasBalancedBrackets("[<hhhiii()}>]") ? "is" : "is not" ) + " balanced" );
+}
