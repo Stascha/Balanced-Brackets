240897b242deea617beae3a76aa4622577b52adc
Stascha <stasarasic@gmail.com>
2018-09-08 18:24:32 +0200

    Update hasBalancedBrackets.java

--- hasBalancedBrackets.java
+++ hasBalancedBrackets.java
@@ -1,44 +1,62 @@
 /** Created by Stasa on 24.11.17.. **/
 
+import java.util.Arrays;
 import java.util.Stack;
 
-public class Brackets{
+public class Zagrade {
 
-public static boolean hasBalancedBrackets(String str){
+    public static boolean hasBalancedBrackets(String str)
+    {
+        Stack<Character> stack = new Stack<Character>();
+        char x, y;
 
-    Stack<Character> stack = new Stack<Character>();
-    char x, y;
+       for(int i = 0; i < str.length(); i++)
+       {
+           x = str.charAt(i);
 
-    for(int i = 0; i < str.length(); i++){
-       x = str.charAt(i);
+       //  if( Arrays.asList('(' , '[' , '{' , '<').contains(x) )
+           if ( x == '(' || x == '[' || x == '{' || x == '<' )
+               stack.push(x);
 
-       if ( x == '(' || x == '[' || x == '{' || x == '<' )
-            stack.push(x);
+           if ( x == ')' || x == ']' || x == '}' || x == '>')
+           {
+                if (stack.empty())
+                    return false;
+                else
+                {
+                    y = stack.pop();
+                    if(x - y != 2 && x - y != 1)
+                        
+                 // if( ( x == ')' && y != '(' ) || 
+                 //     ( x == ']' && y != '[' ) || 
+                 //     ( x == '}' && y != '{' ) || 
+                 //     ( x == '>' && y != '<' ) 
+                      )
 
-       if ( x == ')' || x == ']' || x == '}' || x == '>') {
-            if (stack.empty())
-            return false;
-               
-            else {
-                y = stack.pop();
-             // if( ( x == ')' && y != '(' ) || 
-             //     ( x == ']' && y != '[' ) || 
-             //     ( x == '}' && y != '{' ) || 
-             //     ( x == '>' && y != '<' ) 
-             //   )
-                if(x - y != 2 && x - y != 1)
-                   return false;
-            }
+                      return false;
+                }
+           }
        }
-   }
 
-    return stack.empty() ? true : false;
+     // if (stack.empty())
+     //    return true;
+     // else
+     //    return false;
+     
+     // return stack.empty() ? true : false;
+
+        return stack.empty();
+    }
+
+    public static void main(String[] args)
+    {
+
+        // Zagrade su sve zatvorene
+        System.out.println("Brackets are" + ( hasBalancedBrackets("{([kglfk{<hhii>}ngccdfnj])ppll{}[]}") ? " " : " not" ) + " balanced" );
+
+        // Zagrade nisu sve zatvorene
+        System.out.println("Brackets are" + ( hasBalancedBrackets("[<hhhiii()}>]") ? " " : " not" ) + " balanced" );
+    }
+
 }
 
-public static void main(String[] args)
-{
-    System.out.println("Brackets are " + ( hasBalancedBrackets("{([kglfk{<hhii>}ngccdfnj])ppll{}[]}") ? "is" : "is not" ) + " balanced" );
-
-  
-    System.out.println("Brackets are " + ( hasBalancedBrackets("[<hhhiii()}>]") ? "is" : "is not" ) + " balanced" );
-}
