881154c74d2a442d208ac636412e149b10093920
Stascha <stasarasic@gmail.com>
2022-01-20 18:06:31 +0100

    Update hasBalancedBrackets.java

--- hasBalancedBrackets.java
+++ hasBalancedBrackets.java
@@ -31,6 +31,7 @@ public class Zagrade {
                  //     ( x == ']' && y != '[' ) || 
                  //     ( x == '}' && y != '{' ) || 
                  //     ( x == '>' && y != '<' ) 
+                 //   )
  
                     return false;
                 }
